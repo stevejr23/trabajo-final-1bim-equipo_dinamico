dea4dfc8a149d3fdd1dd645ae53c8f156a653dca
eaalvarado8 <eric_alvarado2202@hotmail.com>
2022-05-29 15:58:13 -0500

    Parte 1

---
+++ configuracion.py
@@ -0,0 +1,11 @@
+# este módulo será usado para posibles configuraciones
+#
+# cadena conector a la base de datos
+#
+# Sqlite
+# cadena_base_datos = 'sqlite:///jugar.db' 
+
+# Mysql
+# para el uso de este dialecto en SqlAlchemy
+# instalar "pip install PyMySQL"
+cadena_base_datos = "mysql+mysqlconnector://root:@localhost:3306/final1bimaa22"
---
+++ ingresa_provincias.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# se importa la clase(s) del
+# archivo genera_tablas
+from crear_tablas import Provincia, Canton
+
+# se importa información del archivo configuracion
+from configuracion import cadena_base_datos
+# se genera enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+provincias = []
+
+with open('data/Listado-Instituciones-Educativas.csv', 'r', encoding="utf8") as archivo:
+    next(archivo, None)
+
+    for r in archivo:
+        r = r.split('|')
+        provincias.append((r[2], r[3]))
