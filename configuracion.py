a781eaa85e53d1df4b91bcfd216c8ab90dac4d44
stevejr.23 <stevej4r412@hotmial.com>
2022-05-30 23:55:51 -0500

    proyecto

--- configuracion.py
+++ configuracion.py
@@ -8,4 +8,4 @@ cadena_base_datos = 'sqlite:///final1bimaa22.db'
 # Mysql
 # para el uso de este dialecto en SqlAlchemy
 # instalar "pip install PyMySQL"
-#cadena_base_datos = "mysql+mysqlconnector://root:@localhost:3308/final1bimaa22"
+#cadena_base_datos = "mysql+mysqlconnector://root:@localhost:3306/final1bimaa22"
